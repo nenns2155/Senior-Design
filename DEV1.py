dc1eb6f915f565ab3cb7de90b9533a84349268d2
Nathan E <nenns@ksu.edu>
2021-09-28 16:28:09 -0500

    Testing

--- DEV1.py
+++ DEV1.py
@@ -8,19 +8,19 @@ GPIO.setup(32,GPIO.OUT) #Motor high low
 #
 
 def motor(feedt):
-    starttime = time.gmtime()
+    starttime = time.time()
     endtime = starttime + feedt
 
     GPIO.output(32,True)
 
     while True:
-        if time.gmtime < endtime:
+        if time.time() < endtime:
             GPIO.output(32,False)
             return
 
 #Test Test Test
 
-feedt = 15 #Length of pulse (in ms) that the motor will run for feeding.
+feedt = 1000 #Length of pulse (in ms) that the motor will run for feeding.
 
 motor(feedt)
 
