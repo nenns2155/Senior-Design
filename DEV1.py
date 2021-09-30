3662a35b579b875c4eae31b36b32fe5abddd8aa0
Nathan E <nenns@ksu.edu>
2021-09-30 14:36:44 -0500

    "pin dev"

--- DEV1.py
+++ DEV1.py
@@ -1,28 +1,35 @@
 import time
 import RPi.GPIO as GPIO
 
+pin = 32
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(32,GPIO.OUT) #Motor high low
+GPIO.setup(pin,GPIO.OUT) #Motor high low
 #
 #
 #
 
-def motor(feedt):
-    starttime = time.time()
-    endtime = starttime + feedt
+### DEV
+GPIO.output(pin,True)
 
-    GPIO.output(32,True)
 
-    while True:
-        if time.time() < endtime:
-            GPIO.output(32,False)
-            return
+
+
+# def motor(feedt):
+#     starttime = time.time()
+#     endtime = starttime + feedt
+
+#     GPIO.output(32,True)
+
+#     while True:
+#         if time.time() < endtime:
+#             GPIO.output(32,False)
+#             return
 
 #Test Test Test
 
-feedt = 1000 #Length of pulse (in ms) that the motor will run for feeding.
+# feedt = 1000 #Length of pulse (in ms) that the motor will run for feeding.
 
-motor(feedt)
+# motor(feedt)
 
 #try:
 #    while True:
