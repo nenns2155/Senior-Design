40a473ebf076e6f0eb322c58a5452f786b1f4cc6
zekejimenez <zekej@ksu.edu>
2021-10-05 15:59:24 -0500

    crontab

---
+++ time_test.py
@@ -0,0 +1,17 @@
+import time
+
+print(time.strftime("%m/%d/%Y %I:%M:%S"))
+i = 0
+entry = ""
+# print(time.ctime(time.time()+3600))
+# while True: 
+#     # beginning of the loop
+#     # loop_start_time = time.time()
+#     # to print the time in the shown format
+#     # print(time.strftime("%m/%d/%Y %I:%M:%S"))
+#     if entry == "":
+#         entry = input()
+#     if entry == (time.strftime("%m/%d/%Y %I:%M%S")):
+#         print(entry)
+#         print ("yea boi")
+#         break
