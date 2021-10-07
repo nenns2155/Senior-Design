a617d3990f00702677a9acdf4f45dfa2ad84f7bc
nenns2155 <nenns2155@gmail.com>
2021-10-07 15:52:13 -0500

    dev

--- Funct_Demo.py
+++ Funct_Demo.py
@@ -11,6 +11,73 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from DEV1 import Feed_Now, Feed_Later, setTime
 
+#########################################################################
+#########################################################################
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO  # import GPIO
+from hx711 import HX711  # import the class HX711
+
+try:
+    GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
+    # Create an object hx which represents your real hx711 chip
+    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
+    hx = HX711(dout_pin=16, pd_sck_pin=19)
+    # measure tare and save the value as offset for current channel
+    # and gain selected. That means channel A and gain 128
+    err = hx.zero()
+    # check if successful
+    if err:
+        raise ValueError('Tare is unsuccessful.')
+
+    reading = hx.get_raw_data_mean()
+    if reading:  # always check if you get correct value or only False
+        # now the value is close to 0
+        print('Data subtracted by offset but still not converted to units:',
+                reading)
+    else:
+        print('invalid data', reading)
+
+    # In order to calculate the conversion ratio to some units, in my case I want grams,
+    # you must have known weight.
+    input('Put known weight on the scale and then press Enter')
+    reading = hx.get_data_mean()
+    if reading:
+        print('Mean value from HX711 subtracted by offset:', reading)
+        known_weight_grams = input(
+            'Write how many grams it was and press Enter: ')
+        try:
+            value = float(known_weight_grams)
+            print(value, 'grams')
+        except ValueError:
+            print('Expected integer or float and I have got:',
+                    known_weight_grams)
+
+        # set scale ratio for particular channel and gain which is
+        # used to calculate the conversion to units. Required argument is only
+        # scale ratio. Without arguments 'channel' and 'gain_A' it sets
+        # the ratio for current channel and gain.
+        ratio = reading / value  # calculate the ratio for channel A and gain 128
+        hx.set_scale_ratio(ratio)  # set ratio for current channel
+        print('Ratio is set.')
+    else:
+        raise ValueError(
+            'Cannot calculate mean value. Try debug mode. Variable reading:', reading)
+
+except (KeyboardInterrupt, SystemExit):
+    print('Bye :)')
+
+def read_weight():
+    for i in range(10):
+        i=i+hx.get_weight_mean(20), 'g')
+    print(i/10)
+    return i/10
+
+    
+#########################################################################
+#########################################################################
+    
+
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -46,7 +113,7 @@ class Ui_MainWindow(object):
 
 
         ##Implementing Buttons
-        self.Feed_Now.clicked.connect(Feed_Now)
+        self.Feed_Now.clicked.connect(read_weight)
         self.Feed_In_30.clicked.connect(lambda: Feed_Later(30))
 
 
