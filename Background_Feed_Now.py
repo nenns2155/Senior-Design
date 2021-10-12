f97c568c4e283dddf66e80c6d360ee03a8810368
Nathan E <nenns@ksu.edu>
2021-10-12 16:10:20 -0500

    dev

--- Background_Feed_Now.py
+++ Background_Feed_Now.py
@@ -4,8 +4,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38,GPIO.OUT)
 
-GPIO.output(motor,1)
+GPIO.output(38,1)
 time.sleep(5)
-GPIO.output(motor,0)
+GPIO.output(38,0)
     
 GPIO.cleanup()
